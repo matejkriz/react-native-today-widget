cf1533e5ec455399b96d9b3bd1140f4c4dcc22fb
Matěj Kříž <Matej@matejkriz.cz>
2017-05-21 19:45:06 +0200

    add postinstall script setting proper CFBundleIdentifier

---
+++ scripts/bundleIdentifier.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -n "$1" ]
+then
+    echo "You provided CFBundleIdentifier: "$1
+    BUNDLE_ID_NEW='"'$1'"'
+else
+    PACKAGE_NAME=$(sed -nE 's/^\s*"name": "(.*?)",$/\1/p' package.json)
+    PLIST_PATH=$(pwd)/ios/${PACKAGE_NAME}/Info.plist
+
+    echo "Reading CFBundleIdentifier from: "$PLIST_PATH
+    BUNDLE_ID=$(defaults read $PLIST_PATH CFBundleIdentifier)
+
+    WIDGET_EXT='.TodayWidgetExtension'
+    BUNDLE_ID_NEW='"'$BUNDLE_ID$WIDGET_EXT'"'
+fi
+echo "setting "$BUNDLE_ID_NEW" as CFBundleIdentifier for TodayWidgetExtension"
+defaults write $(pwd)/node_modules/react-native-today-widget/ios/TodayWidgetExtension/Info.plist CFBundleIdentifier $BUNDLE_ID_NEW

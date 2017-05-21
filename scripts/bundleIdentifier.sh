88ec7fa5fd50c82515ec4892584cb52fbfc165b7
Matěj Kříž <Matej@matejkriz.cz>
2017-05-21 20:12:05 +0200

    fix path to parent Info.plist file from postinstall script

--- scripts/bundleIdentifier.sh
+++ scripts/bundleIdentifier.sh
@@ -2,17 +2,17 @@
 
 if [ -n "$1" ]
 then
-    echo "You provided CFBundleIdentifier: \n\e[7"$1"\n"
+    echo "You provided CFBundleIdentifier: "$1
     BUNDLE_ID_NEW='"'$1'"'
 else
     PACKAGE_NAME=$(sed -nE 's/^\s*"name": "(.*?)",$/\1/p' ../../package.json)
-    PLIST_PATH=$(pwd)/ios/${PACKAGE_NAME}/Info.plist
+    PLIST_PATH=$(pwd)../../ios/${PACKAGE_NAME}/Info.plist
 
-    echo "Reading CFBundleIdentifier from: \n"$PLIST_PATH"\n"
+    echo "Reading CFBundleIdentifier from: "$PLIST_PATH
     BUNDLE_ID=$(defaults read $PLIST_PATH CFBundleIdentifier)
 
     WIDGET_EXT='.TodayWidgetExtension'
     BUNDLE_ID_NEW='"'$BUNDLE_ID$WIDGET_EXT'"'
 fi
-echo "setting \e[7"$BUNDLE_ID_NEW"\e[24m as CFBundleIdentifier for TodayWidgetExtension\n"
+echo "setting "$BUNDLE_ID_NEW" as CFBundleIdentifier for TodayWidgetExtension"
 defaults write $(pwd)/node_modules/react-native-today-widget/ios/TodayWidgetExtension/Info.plist CFBundleIdentifier $BUNDLE_ID_NEW

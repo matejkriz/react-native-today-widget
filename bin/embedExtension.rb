598e11c313ffcd098c133abc43f12b40fa212b0c
Matěj Kříž <Matej@matejkriz.cz>
2017-06-04 16:56:05 +0200

    add script to embed extension binaries to main app (WIP)

---
+++ bin/embedExtension.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'xcodeproj'
+puts 'Starting ruby script'
+puts __FILE__
+
+project_location = '../../**'
+binary_name = 'TodayWidgetExtension.appex'
+
+# Get useful variables
+project = Xcodeproj::Project.find(project_location)
+puts 'Finishing ruby script'
+
+# Save Xcode project
+project.save

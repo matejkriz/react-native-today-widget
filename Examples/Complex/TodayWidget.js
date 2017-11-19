649f6e5164f72e0989dbc7a9e808918cf11939e0
Matěj Kříž <Matej@matejkriz.cz>
2017-11-19 12:05:59 +0100

    init TodayWidget root file registered on TodayWidgetExtension

---
+++ Examples/Complex/TodayWidget.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const TodayWidget = () => (
+  <View>
+    <Text>HoppyGo</Text>
+  </View>
+);
+
+export default TodayWidget;
--- Examples/Complex/index.js
+++ Examples/Complex/index.js
@@ -1,4 +1,8 @@
-import { AppRegistry } from 'react-native';
+import { Platform, AppRegistry } from 'react-native';
 import App from './App';
+import TodayWidget from './TodayWidget';
 
 AppRegistry.registerComponent('HoppyGoWidget', () => App);
+if (Platform.OS === 'ios') {
+  AppRegistry.registerComponent('TodayWidgetExtension', () => TodayWidget);
+}

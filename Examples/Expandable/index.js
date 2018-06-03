a1347cb67101353a1b54d305c369cf9be48d7c65
Matěj Kříž <Matej@matejkriz.cz>
2018-06-03 14:43:18 +0200

    upgrade Expandable example to use lates versions of RN and RNTW

--- Examples/Expandable/index.js
+++ Examples/Expandable/index.js
@@ -4,13 +4,8 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, { Component } from "react";
+import { AppRegistry, StyleSheet, Text, View } from "react-native";
 
 export default class Expandable extends Component {
   render() {
@@ -20,11 +15,11 @@ export default class Expandable extends Component {
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.android.js
+          To get started, edit index.ios.js
         </Text>
         <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
+          Press Cmd+R to reload,{"\n"}
+          Cmd+D or shake for dev menu
         </Text>
       </View>
     );
@@ -34,20 +29,20 @@ export default class Expandable extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
 
-AppRegistry.registerComponent('Expandable', () => Expandable);
+AppRegistry.registerComponent("Expandable", () => Expandable);
